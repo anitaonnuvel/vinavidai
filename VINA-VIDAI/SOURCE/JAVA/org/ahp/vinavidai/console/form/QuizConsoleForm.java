30c5539b8d3349187e13feb95eaf6ad6f4611d98
Anita Onnuvel <anitaonnuvel@gmail.com>
2012-09-18 23:48:47 -0500

    format change

--- VINA-VIDAI/SOURCE/JAVA/org/ahp/vinavidai/console/form/QuizConsoleForm.java
+++ VINA-VIDAI/SOURCE/JAVA/org/ahp/vinavidai/console/form/QuizConsoleForm.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Anita Onnuvel
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,24 +27,23 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @author Anita Onnuvel
- * 
+ *
  * @struts.form name="QuizConsoleForm"
  */
 public class QuizConsoleForm extends AhpAbstractForm {
 
     private static final long serialVersionUID = 1L;
 
-    final static Logger LOGGER = LoggerFactory
-            .getLogger( QuizConsoleForm.class );
+    final static Logger LOGGER = LoggerFactory.getLogger( QuizConsoleForm.class );
 
     @Override
     protected void resetForm( ActionMapping pActionMapping,
-            HttpServletRequest pHttpServletRequest ) {
+                              HttpServletRequest pHttpServletRequest ) {
     }
 
     @Override
     public ActionErrors validate( ActionMapping pActionMapping,
-            HttpServletRequest pHttpServletRequest ) {
+                                  HttpServletRequest pHttpServletRequest ) {
         ActionErrors lActionErrors = new ActionErrors();
         String lSubmitAction = StringUtils.trimToNull( this.getSubmitAction() );
         if ( lSubmitAction != null
