bfde693c947140b0098ce91a1097cf96509281ba
Anita Onnuvel <anitaonnuvel@gmail.com>
2012-09-20 17:24:08 -0500

    test

--- VINA-VIDAI/SOURCE/JAVA/org/ahp/configuration/pojo/AhpMessageResource.java
+++ VINA-VIDAI/SOURCE/JAVA/org/ahp/configuration/pojo/AhpMessageResource.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Anita Onnuvel
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,9 +22,9 @@ import org.ahp.configuration.pojo.enums.AhpMessageResourceLoadType;
 import org.ahp.configuration.pojo.enums.AhpMessageResourceType;
 
 /**
- * 
+ *
  * @author Anita Onnuvel
- * 
+ *
  */
 public class AhpMessageResource implements Serializable {
 
@@ -74,6 +74,8 @@ public class AhpMessageResource implements Serializable {
         mResource = pResource;
     }
 
+    /***
+    */
     public String toString() {
         return AhpStringUtil.reflectionToString( this );
     }
